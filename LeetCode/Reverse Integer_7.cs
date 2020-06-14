9ce5ff4a84ae7e370132da96630485b3ec15bb44
william870708 <willy870708@gmail.com>
2020-06-14 21:08:26 +0800

    add question7

---
+++ LeetCode/Reverse Integer_7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Reverse_Integer_7
+    {
+        public int Reverse(int x)
+        {
+            var ans = 0;
+            while(x != 0)
+            {
+                int pop = x % 10;
+                x /= 10;
+                if (ans > int.MaxValue/ 10 || (ans == int.MaxValue / 10 && pop > 7)) return 0;
+                if (ans < int.MinValue / 10 || (ans == int.MinValue / 10 && pop < -8)) return 0;
+                ans = ans * 10 + pop;                
+            }  
+            return ans;
+        }
+    }
+}
