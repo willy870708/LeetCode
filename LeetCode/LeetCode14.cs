734b28b7cb7c67b9667aae9b4552b7d28db73267
william870708 <willy870708@gmail.com>
2020-06-14 18:54:48 +0800

    add project

---
+++ LeetCode/LeetCode14.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class LeetCode14
+    {
+        static void Main(string[] args)
+        {
+            var ans = LongestCommonPrefix(
+                new string[]{
+                "flower","flow","flight"
+                });
+            Console.Write(ans);
+
+        }
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            var ans = "";
+            int? min = strs.Min(x => x.Length);
+            for (int i = 0; i < min; i++)
+            {
+                var first = strs[0][i];
+                if (strs.All(x => x[i] == first))
+                {
+                    ans += first;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return ans;
+        }
+    }
+}
