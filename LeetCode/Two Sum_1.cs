dde65654c614f2912c168ed61105ff0fccbd504c
william870708 <willy870708@gmail.com>
2020-06-14 20:01:04 +0800

    add question1

---
+++ LeetCode/Two Sum_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Two_Sum_1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var ans = new int[2];
+            var temp = nums.ToList();
+            for (int i = 0; i < temp.Count - 1; i++)
+            {
+                for (int j = i+1; j < temp.Count; j++)
+                {
+                    if (target - temp[i] == temp[j])
+                    {
+                        ans = new int[2] { i, j };
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
