e5847bc0a89ffa6c4a8c7d0c7c9842ffc6c1fc90
william870708 <willy870708@gmail.com>
2020-06-15 23:09:27 +0800

    add question 7 unit test

---
+++ UnitTest/Reverse Integer_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    class Reverse_Integer_Test
+    {
+        [Test]
+        public void Reverse_integer_test()
+        {
+            var reverse_integer = new Reverse_Integer_7();
+            var expected = reverse_integer.Reverse(123456);
+            Assert.AreEqual(expected, 654321); 
+        }
+    }
+}
