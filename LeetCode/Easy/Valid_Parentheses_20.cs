f9b2d3de19edc70769e16abe353c4695c660de6b
william870708 <willy870708@gmail.com>
2020-06-18 23:31:00 +0800

    add question 20 and unit test

---
+++ LeetCode/Easy/Valid_Parentheses_20.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class Valid_Parentheses_20
+    {
+        public bool IsValid(string s)
+        {
+            var temp = new Stack();
+            if (s.Length == 1) return false;
+            if (s.Length == 0) return true;
+            temp.Push(s[0]);
+            var dictionary = new Dictionary<char, char>()
+            {
+                {'(',')' },
+                {'{','}' },
+                {'[',']' },
+                {')','(' },
+                {']','[' },
+                {'}','{' },
+            };
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == dictionary.Keys.ToList()[0]
+                        || s[i] == dictionary.Keys.ToList()[1]
+                        || s[i] == dictionary.Keys.ToList()[2])
+                {
+                    temp.Push(s[i]);
+                }
+                else if(temp.Count == 0)
+                {
+                    temp.Push(s[i]);
+                }
+                else if(s[i] != dictionary[Convert.ToChar(temp.Peek())])
+                {
+                    return false;
+                }
+                if (s[i] == dictionary[Convert.ToChar(temp.Peek())])
+                {
+                    temp.Pop();
+                }
+            }
+            if(temp.Count!=0)return false;
+            return true;
+        }
+    }
+}
---
+++ UnitTest/Easy/Valid_Parentheses_Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Easy;
+using NUnit.Framework;
+
+namespace UnitTest.Easy
+{
+    class Valid_Parentheses_Test
+    {
+        [Test]
+        public void valid_parentheses_pass_test()
+        {
+            var valid_parentheses = new Valid_Parentheses_20();
+            var isValid = valid_parentheses.IsValid("{()}");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void valid_parentheses_fail_test()
+        {
+            var valid_parentheses = new Valid_Parentheses_20();
+            var isValid = valid_parentheses.IsValid("[])");
+            Assert.IsFalse(isValid);
+        }
+    }
+}
