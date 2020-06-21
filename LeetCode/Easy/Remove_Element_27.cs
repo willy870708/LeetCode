3b1ab715d67cf01693f153b53a5aa4f4b00556f6
william870708 <willy870708@gmail.com>
2020-06-21 15:17:54 +0800

    add question 27 and unit test

---
+++ LeetCode/Easy/Remove_Element_27.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class Remove_Element_27
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            var ans = nums.Length;
+            for (int i = 0; i < ans; i++)
+            {
+                if(nums[i] == val)
+                {
+                    nums[i] = nums[ans-1];
+                    ans--;
+                }
+            }
+                return ans;
+        }
+    }
+}
---
+++ UnitTest/Easy/Remove_Element_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Easy;
+using NUnit.Framework;
+
+namespace UnitTest.Easy
+{
+    class Remove_Element_Test
+    {
+        [Test]
+        public void remove_Element_Test()
+        {
+            var remove = new Remove_Element_27();
+            var expected = remove.RemoveElement(new int[] { 3, 2, 2, 3 },3);
+            Assert.AreEqual(expected, 2);
+        }
+    }
+}
