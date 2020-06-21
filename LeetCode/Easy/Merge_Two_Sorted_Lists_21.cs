d578336150d5595d37173b3935cf041924f3fe3a
william870708 <willy870708@gmail.com>
2020-06-21 13:42:12 +0800

    add question 21 and uint test

---
+++ LeetCode/Easy/Merge_Two_Sorted_Lists_21.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class Merge_Two_Sorted_Lists_21
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            var head = new ListNode();
+            var prev = head;
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val < l2.val)
+                {
+                    prev.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    prev.next = l2;
+                    l2 = l2.next;
+                }
+                prev = prev.next;
+            }
+            if (l1 != null)
+            {
+                prev.next = l1;
+            }
+            if (l2 != null)
+            {
+                prev.next = l2;
+            }
+            return head.next;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
---
+++ UnitTest/Easy/Merge_Two_Sorted_Lists_Test.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using LeetCode.Easy;
+using NUnit.Framework;
+
+namespace UnitTest.Easy
+{
+    class Merge_Two_Sorted_Lists_Test
+    {
+        [Test]
+        public void When_Merge_Two_Sorted_Lists_()
+        {
+            var merge = new Merge_Two_Sorted_Lists_21();
+            var expected = merge.MergeTwoLists(new ListNode() {
+                val = 1,
+                next = new ListNode()
+                {
+                    val = 2,
+                    next = new ListNode()
+                    {
+                        val = 4,
+                        next = null
+                    }
+                }
+            }, new ListNode() {
+                val = 1,
+                next = new ListNode()
+                {
+                    val = 3,
+                    next = new ListNode()
+                    {
+                        val = 4,
+                        next = null
+                    }
+                }
+            });
+            expected.Should().BeEquivalentTo(new ListNode()
+            {
+                val = 1,
+                next = new ListNode()
+                {
+                    val = 1,
+                    next = new ListNode()
+                    {
+                        val = 2,
+                        next = new ListNode()
+                        {
+                            val = 3,
+                            next = new ListNode()
+                            {
+                                val = 4,
+                                next = new ListNode()
+                                {
+                                    val = 4,
+                                    next = null
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+            }
+    }
+}
