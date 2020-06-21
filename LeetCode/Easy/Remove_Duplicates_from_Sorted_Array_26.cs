12d77d447640a6e1aa3a5c87bba321e9cab8d334
william870708 <willy870708@gmail.com>
2020-06-21 14:41:05 +0800

    add question 26 and unit test

---
+++ LeetCode/Easy/Remove_Duplicates_from_Sorted_Array_26.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class Remove_Duplicates_from_Sorted_Array_26
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            int i = 0;
+            for(int j = 1; j < nums.Length; j++)
+            {
+                if(nums[i] != nums[j])
+                {
+                    i++;
+                }
+            }
+            return i + 1;
+        }
+    }
+}
---
+++ UnitTest/Easy/Remove_Duplicates_from_Sorted_Array_Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Easy;
+using NUnit.Framework;
+
+namespace UnitTest.Easy
+{
+    class Remove_Duplicates_from_Sorted_Array_Test
+    {
+        [Test]
+        public void remove_duplicates_test()
+        {
+            var remove_duplicate = new Remove_Duplicates_from_Sorted_Array_26();
+            var expected = remove_duplicate.RemoveDuplicates(new int[] {1, 1, 2});
+            Assert.AreEqual(expected, 2);
+        }
+    }
+}
