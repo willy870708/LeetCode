65add82ff9c1dfee1466a24f1bc17804abf6a9c0
william870708 <willy870708@gmail.com>
2020-06-22 23:07:43 +0800

    add question 28 and unit test

---
+++ LeetCode/Easy/Implement_strStr_28.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Easy
+{
+    public class Implement_strStr_28
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            var i = 0;
+            var ans = 0;
+            var state = false;
+            if (needle.Length > haystack.Length) return -1;
+            if ((haystack.Length == 0 && needle.Length == 0) || needle.Length == 0) return 0;
+            for (i = 0; i < haystack.Length; i++)
+            {
+                if (ans == needle.Length)
+                {
+                    break;
+                }
+                if (i + needle.Length > haystack.Length) return -1;
+                if (haystack[i] == needle[0])
+                {
+                    if (haystack.Substring(i, needle.Length) == needle)
+                    {
+                        return i;
+                    }
+                }
+            }
+            if (state == false)
+            {
+                return -1;
+            }
+            return ans;
+        }
+    }
+}
---
+++ UnitTest/Easy/Implement_str_test.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Easy;
+using NUnit.Framework;
+
+namespace UnitTest.Easy
+{
+    class Implement_str_test
+    {
+        [Test]
+        public void implement_str__succes_test()
+        {
+            var implement = new Implement_strStr_28();
+            var expected = implement.StrStr("mississippi", "sippia");
+            Assert.AreEqual(expected, 1);
+        }
+        [Test]
+        public void implement_str__fail_test()
+        {
+            var implement = new Implement_strStr_28();
+            var expected = implement.StrStr("aaaaaaaa", "bba");
+            Assert.AreEqual(expected, -1);
+        }
+    }
+}
