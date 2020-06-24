cb5726d3766166ba2419986c66e7b218a4d086bb
william870708 <willy870708@gmail.com>
2020-06-24 23:40:23 +0800

    add question 38 and unit test

---
+++ LeetCode/Easy/Count_and_Say_38.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class Count_and_Say_38
+    {
+        public string CountAndSay(int n)
+        {
+            if (n == 1) return "1";
+            var countSay = "1";
+            for (var i = 2; i <= n; i++)
+            {
+                var count = 1;
+                var num = countSay[0];
+                var temp = countSay;
+                countSay = "";
+                for (var a = 1; a < temp.Length; a++)
+                {
+                    if (temp[a] == num)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        countSay += count;
+                        countSay += num;
+                        num = temp[a];
+                        count = 1;
+                    }
+                }
+                countSay += count;
+                countSay += num;
+            }
+            return countSay;
+        }
+    }
+}
---
+++ UnitTest/Easy/Count_and_Say_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Easy;
+using NUnit.Framework;
+
+namespace UnitTest.Easy
+{
+    class Count_and_Say_Test
+    {
+        [Test]
+        public void count_and_say()
+        {
+            var count = new Count_and_Say_38();
+            var expected = count.CountAndSay(4);
+            Assert.AreEqual(expected, "1211");
+        }
+    }
+}
