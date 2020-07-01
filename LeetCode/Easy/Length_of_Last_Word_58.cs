5535f083965f76933e72d1a490f2a2f922e29b13
william870708 <willy870708@gmail.com>
2020-07-01 22:31:26 +0800

    add question 58 and unit test

---
+++ LeetCode/Easy/Length_of_Last_Word_58.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class Length_of_Last_Word_58
+    {
+        public int LengthOfLastWord(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+            var temp = s.Split(' ');
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if(temp.Last().Length == 0)
+                {
+                    temp = temp.Where(val => val != temp.Last()).ToArray();
+                }
+                else
+                {
+                    return temp.Last().Length;
+                }
+            }
+            if (temp.Length == 0) return 0;
+            return temp.Last().Length;
+        }
+    }
+}
---
+++ UnitTest/Easy/Length_of_Last_Word_Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Easy;
+using NUnit.Framework;
+
+namespace UnitTest.Easy
+{
+    class Length_of_Last_Word_Test
+    {
+        [Test]
+        public void length_of_last_word()
+        {
+            var length = new Length_of_Last_Word_58();
+            var expected = length.LengthOfLastWord("b   a    ");
+            Assert.AreEqual(expected, 1);
+        }
+    }
+}
