353d483324deff8a7cbc3a05bb2eb1bbcb9603cd
william870708 <willy870708@gmail.com>
2020-07-02 23:45:44 +0800

    add question 66 and unit test

---
+++ LeetCode/Easy/Plus_One_66.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class Plus_One_66
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            int carry = 0;
+            if (digits.Last() == 9)
+            {
+                digits[digits.Count() - 1] = 0;
+                carry++;
+            }
+            else
+            {
+                digits[digits.Count() - 1] += 1;
+            }
+            for (int i = digits.Count() - 2; i >= 0; i--)
+            {
+                if (carry == 1)
+                {
+                    if (digits[i] == 9)
+                    {
+                        digits[i] = 0;
+                    }
+                    else
+                    {
+                        digits[i] += 1;
+                        carry--;
+                    }
+                }
+            }
+            if(carry == 1)
+            {
+                var ans = new int[digits.Count() + 1];
+                new int[] { 1 }.CopyTo(ans, 0);
+                digits.CopyTo(ans, 1);
+                return ans;
+            }
+            return digits;
+        }
+    }
+}
---
+++ UnitTest/Easy/Plus_One_Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Easy;
+using NUnit.Framework;
+
+namespace UnitTest.Easy
+{
+    class Plus_One_Test
+    {
+        [Test]
+        public void plus_one_test()
+        {
+            var plus = new Plus_One_66();
+            var expected = plus.PlusOne(new int[] { 9 });
+            Assert.AreEqual(expected, new int[] { 1,0 });
+        }
+    }
+}
